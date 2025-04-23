7b3a58b9df78df3155853ad136a191f79de43d20
Saiful Islam <saiful@co.design>
2025-04-23 10:34:28 +0600

    update

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,18 @@ import express from "express";
 const app = express();
 const PORT = 8080;
 
-app.get("/", (req, res) => {
+app.get("/", (req: any, res: { send: (arg0: string) => void }) => {
   res.send("This is Home Page");
 });
 
-app.get("/product", (req, res) => {
+app.get("/product", (req: any, res: { send: (arg0: string) => void }) => {
   res.send("This is Product Page");
 });
 
-app.get("/order", (req, res) => {
+app.get("/order", (req: any, res: { send: (arg0: string) => void }) => {
   res.send("This is order Page");
 });
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
-app.get("/product", (req, res) => {
-  res.send("This is Product Page");
-})
